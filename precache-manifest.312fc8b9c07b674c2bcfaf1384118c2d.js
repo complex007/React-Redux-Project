4b1f04e794377516c3e2da5ab949fff634623141
Yuqi <yuqi.wang@Alpha7.co>
2018-11-26 10:49:55 +0800

    Updates

---
+++ precache-manifest.312fc8b9c07b674c2bcfaf1384118c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8595219a1650de135163",
+    "url": "/React-Redux-Project/static/js/runtime~main.8595219a.js"
+  },
+  {
+    "revision": "74861fb0469974aec382",
+    "url": "/React-Redux-Project/static/js/main.74861fb0.chunk.js"
+  },
+  {
+    "revision": "c52f75b362f8039d247a",
+    "url": "/React-Redux-Project/static/js/1.c52f75b3.chunk.js"
+  },
+  {
+    "revision": "74861fb0469974aec382",
+    "url": "/React-Redux-Project/static/css/main.1e79c294.chunk.css"
+  },
+  {
+    "revision": "c52f75b362f8039d247a",
+    "url": "/React-Redux-Project/static/css/1.023b8769.chunk.css"
+  },
+  {
+    "revision": "368145b67746c8c650ffc349595f4f5b",
+    "url": "/React-Redux-Project/index.html"
+  }
+];
